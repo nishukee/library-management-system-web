eecd0f367694e8acb7421943c9680479b491e039
nishukee <nishchaymnayak@gmail.com>
2021-08-02 11:34:55 +0530

    Added Downloaded Bootstrap css and js

--- index.php
+++ index.php
@@ -7,7 +7,6 @@ include_once 'php/config.php';
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
         <link rel="stylesheet" href="css/styles.css">
         <title>Login Page</title>
     </head>
